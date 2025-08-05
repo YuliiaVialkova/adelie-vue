0b2b518c6fb7028ad7142bbdd85a1d6f040e60e3
Yuliia Vialkova <julia.zagrafova@gmail.com>
2025-08-05 15:58:05 +0300

    SASS installed

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <header class="app-header">
+    <p>Logo</p>
+  </header>
+</template>
+
+<style scoped lang="scss">
+.app-header {
+  p {
+    background-color: aqua;
+  }
+}
+</style>
