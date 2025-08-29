ecb8f4381f8e34fd3634e59f295edf7a5bbfbdb0
Yuliia Vialkova <julia.zagrafova@gmail.com>
2025-08-29 16:42:59 +0300

    created new component AppFigcaption and added into AppArticle

--- src/components/AppArticle.vue
+++ src/components/AppArticle.vue
@@ -1,7 +1,6 @@
 <script setup>
-import IconDate from '@/components/icons/IconDate.vue'
-import IconAuthor from '@/components/icons/IconAuthor.vue'
 import AppContainer from '@/components/AppContainer.vue'
+import AppFigcaption from '@/components/AppFigcaption.vue'
 import { RouterLink } from 'vue-router'
 
 defineProps({
@@ -38,7 +37,7 @@ defineProps({
 
 <template>
   <article class="app-article">
-    <AppContainer class="app-article__container container--small">
+    <AppContainer class="app-article__container app-container--small">
       <figure class="app-article__image-wrapper">
         <img
           class="app-article__image"
@@ -48,15 +47,10 @@ defineProps({
             ${images.jpg2x} 2x`"
           :alt="images.alt"
         />
-
-        <figcaption class="app-article__meta meta">
-          <time :datetime="dateISO" class="meta__date"
-            ><IconDate class="meta__icon icon icon--date" />{{ formattedDate }}</time
-          >
-          <span class="meta__author">
-            <IconAuthor class="meta__icon icon icon--author" />{{ author }}</span
-          >
-        </figcaption>
+        <AppFigcaption class="app-article__figcaption" :datetime="dateISO">
+          <template #date>{{ formattedDate }}</template>
+          <template #author>{{ author }}</template>
+        </AppFigcaption>
       </figure>
       <h2 class="app-article__title">
         <RouterLink :to="link" class="app-article__link">{{ title }}</RouterLink>
@@ -72,62 +66,39 @@ defineProps({
 </template>
 <style lang="scss">
 .app-article {
-  &__container {
-    .app-article__image-wrapper {
-      position: relative;
-      margin: 0 auto;
-      min-height: 230px;
-      min-width: 355px;
-      aspect-ratio: 355/230;
-      img {
-        display: block;
-        height: 100%;
-        width: 100%;
-        object-fit: cover;
-      }
-      .app-article__meta {
-        background-color: var(--main-bg);
-        position: absolute;
-        bottom: -1px;
-        left: -1px;
-        display: flex;
-        flex-wrap: wrap;
-        align-items: center;
-        justify-content: space-between;
-        gap: 10px 50px;
-        font-size: 17px;
-        line-height: 1.29;
-        padding: toRem(21) toRem(20) toRem(16);
-        time,
-        span {
-          display: flex;
-          align-items: flex-start;
-          justify-content: center;
-          gap: 10px;
-        }
-      }
-    }
-
-    h2 {
-      font-size: 23px;
-      line-height: 1.739;
-      margin: 10px 0 10px;
-    }
-    h2 + p {
-      font-size: 16px;
-      line-height: 1.56;
-      margin-bottom: 12px;
-    }
-    .app-article__button {
-      font-size: 20px;
-      font-weight: 500;
-      line-height: 1.75;
-      text-transform: uppercase;
-    }
-    .app-article__button.highlight::after {
-      bottom: 9%;
-      height: 41%;
-    }
+  &__image-wrapper {
+    position: relative;
+    margin: 0 auto;
+    min-height: 230px;
+    min-width: 355px;
+    aspect-ratio: 355/230;
   }
+  &__image {
+    display: block;
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
+  }
+}
+
+h2 {
+  font-size: 23px;
+  line-height: 1.739;
+  margin: 10px 0 10px;
+}
+h2 + p {
+  font-size: 16px;
+  line-height: 1.56;
+  margin-bottom: 12px;
+}
+.app-article__button {
+  font-size: 20px;
+  font-weight: 500;
+  line-height: 1.75;
+  text-transform: uppercase;
+}
+.app-article__button.highlight::after {
+  bottom: 9%;
+  height: 41%;
 }
 </style>
---
+++ src/components/AppFigcaption.vue
@@ -0,0 +1,43 @@
+<script setup>
+import IconDate from '@/components/icons/IconDate.vue'
+import IconAuthor from '@/components/icons/IconAuthor.vue'
+</script>
+
+<template>
+  <figcaption class="app-figcaption">
+    <time class="app-figcaption__date"
+      ><IconDate class="app-figcaption__icon icon app-figcaption__icon--date" /><slot
+        name="date"
+      ></slot
+    ></time>
+    <span class="app-figcaption__author">
+      <IconAuthor class="app-figcaption__icon icon app-figcaption__icon--author" /><slot
+        name="author"
+      ></slot>
+    </span>
+  </figcaption>
+</template>
+
+<style lang="scss">
+.app-figcaption {
+  background-color: var(--main-bg);
+  position: absolute;
+  bottom: -1px;
+  left: -1px;
+  display: flex;
+  flex-wrap: wrap;
+  align-items: center;
+  justify-content: space-between;
+  gap: 10px 50px;
+  font-size: 17px;
+  line-height: 1.29;
+  padding: toRem(21) toRem(20) toRem(16);
+  &__date,
+  &__author {
+    display: flex;
+    align-items: flex-start;
+    justify-content: center;
+    gap: 10px;
+  }
+}
+</style>
